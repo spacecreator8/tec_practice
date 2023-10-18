6e340e20435fc91cc28474978b45c6dc0a5da39e
spacecreator8 <spcc888@ya.ru>
2023-10-19 05:43:34 +0700

    Сделал 7 пункт

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -9,12 +9,18 @@ def index(request):
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     num_authors = Author.objects.count()
 
-    return render(
-        request,
-        'index.html',
-        context={'num_books': num_books, 'num_instances': num_instances,
-                 'num_instances_available': num_instances_available, 'num_authors': num_authors},
-    )
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_visits': num_visits,
+    }
+
+    return render(request, 'index.html', context=context)
 
 
 class AuthorsListView(generic.ListView):
