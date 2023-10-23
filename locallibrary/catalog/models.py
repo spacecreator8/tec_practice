778e95acf8fa5789a404aa92355d72671f7f77da
spacecreator8 <spcc888@ya.ru>
2023-10-23 13:40:28 +0700

    10

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -5,7 +5,6 @@ import uuid  # Required for unique book instances
 from datetime import date
 
 
-
 class Genre(models.Model):
     name = models.CharField(max_length=200, help_text="Enter a book genre (e.g. Science Fiction, French Poetry etc.)")
 
@@ -20,6 +19,7 @@ class Book(models.Model):
     isbn = models.CharField('ISBN', max_length=13,
                             help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
     genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
+    language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True, related_name='language', blank=True)
 
     def __str__(self):
         return self.title
@@ -35,8 +35,10 @@ class Book(models.Model):
 
 
 
+
 class BookInstance(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4,help_text="Unique ID for this particular book across whole library")
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                          help_text="Unique ID for this particular book across whole library")
     book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
@@ -75,3 +77,10 @@ class Author(models.Model):
 
     def __str__(self):
         return '%s, %s' % (self.last_name, self.first_name)
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
